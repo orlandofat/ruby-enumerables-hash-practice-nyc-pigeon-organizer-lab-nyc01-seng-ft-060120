532b2e33f8eedf2c8e24013915f8c74e0e950ae1
Orlando Challenger <orlandofat@gmail.com>
2020-05-20 20:26:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  #pigeon_data 
+  
+  new_data = data.each_with_object({}) do |(key, value), new_data|
+    value.each do |inner_key, name|
+      name.each do |name|
+        if !new_data[name]
+          new_data[name] = {}
+        end
+        if !new_data[name][key]
+          !new_data[name][key] = []
+        end
+        new_data[name][key].push(inner_key.to_s)
+      end
+    end
+  end
+  binding.pry
 end
